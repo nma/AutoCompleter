a140e1363056dec9eb07828e6b7fdb6730726e4b
nma <nickma38@gmail.com>
2015-07-21 10:15:20 -0700

    initial project setup

---
+++ src/com/fun/Main.java
@@ -0,0 +1,8 @@
+package com.fun;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ test/com/fun/Preprocessor.java
@@ -0,0 +1,7 @@
+package com.fun;
+
+/**
+ * Created by nickma on 2015-07-21.
+ */
+public class Preprocessor {
+}
